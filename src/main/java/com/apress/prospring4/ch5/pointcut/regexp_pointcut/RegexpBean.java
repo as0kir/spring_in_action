afee4432d59660e20388cb5a8a2f404e6d39ae6c
askir@inbox.ru <12askir!scyzm>
2018-10-16 15:45:20 +1000

    pointcut

---
+++ src/main/java/com/apress/prospring4/ch5/pointcut/regexp_pointcut/RegexpBean.java
@@ -0,0 +1,15 @@
+package com.apress.prospring4.ch5.pointcut.regexp_pointcut;
+
+public class RegexpBean {
+    public void foo1(){
+        System.out.println("foo1()");
+    }
+
+    public void foo2(){
+        System.out.println("foo2()");
+    }
+
+    public void bar(){
+        System.out.println("bar()");
+    }
+}
