118bda8de781420c2574cfd78f0bbc90518b9659
askir@inbox.ru <12askir!scyzm>
2018-11-01 11:09:00 +1000

    spring data jpa

--- src/main/java/com/apress/prospring4/ch8/spring_data/repository/ContactRepository.java
+++ src/main/java/com/apress/prospring4/ch8/spring_data/repository/ContactRepository.java
@@ -2,12 +2,10 @@ package com.apress.prospring4.ch8.spring_data.repository;
 
 import com.apress.prospring4.ch8.spring_data.entities.Contact;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface ContactRepository extends CrudRepository<Contact, Long> {
 //    List<Contact> findByFirstName(String firstName);
 //    List<Contact> findByFirstNameAndLastName(String firstName, String lastName);
